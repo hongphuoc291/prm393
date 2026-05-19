5133c0b364949683f8dda1f14a81e6c930352abe
Vuong Hong Phuoc <vuonghongphuoc291@gmail.com>
2026-05-19 11:46:36 +0700

    update func

--- lab1/lib/Entities/Product.dart
+++ lab1/lib/Entities/Product.dart
@@ -15,4 +15,50 @@ class Product{
       print("ID: ${p.id} - Name: ${p.name} - Price: ${p.price}");
     }
   }
+ //sắp xếp giá tăng
+  static List<Product> sortByPriceAsc() {
+    List<Product> result = [...products];
+    result.sort((a, b) => a.price.compareTo(b.price));
+    return result;
+  }
+
+  //sắp xếp giá giảm
+  static List<Product> sortByPriceDesc() {
+    List<Product> result = [...products];
+    result.sort((a, b) => b.price.compareTo(a.price));
+    return result;
+  }
+
+  //add
+  static void addProduct(Product p) {
+    products.add(p);
+  }
+
+
+  // Delete product by Id
+  static void deleteProduct(int id) {
+    products.removeWhere((p) => p.id == id);
+  }
+
+  // Update product by Id
+  static void updateProduct(int id, String newName, double newPrice) {
+    int index = products.indexWhere((p) => p.id == id);
+    if (index != -1) {
+      products[index] = Product(
+          id: id,
+          name: newName,
+          image: products[index].image,
+          price: newPrice
+      );
+    } else {
+      print("Can not found product ID: $id");
+    }
+  }
+
+  // Search by name
+  static List<Product> searchByName(String keyword) {
+    return products
+        .where((p) => p.name.toLowerCase().contains(keyword.toLowerCase()))
+        .toList();
+  }
 }
--- lab1/lib/main.dart
+++ lab1/lib/main.dart
@@ -3,4 +3,40 @@ import 'package:lab1/Entities/Product.dart';
 void main() {
   print("List Product");
   Product.showAll();
+
+  print("Product list sorted by ascending price");
+  var listAsc = Product.sortByPriceAsc();
+  listAsc.forEach((p) => print("${p.name} - ${p.price}"));
+
+  print("Product list sorted by descending price");
+  var listDesc = Product.sortByPriceDesc();
+  listDesc.forEach((p) => print("${p.name} - ${p.price}"));
+
+//add
+  Product.addProduct(Product(id: 3, name: "Apple", image: "apple.png", price: 23500,
+  ));
+
+  print("List Product");
+  Product.showAll();
+
+
+  print("Update successful");
+  Product.updateProduct(1, "Premium Peach", 60000);
+  Product.showAll();
+
+  // delete
+  print("Delete successful");
+  Product.deleteProduct(2);
+  Product.showAll();
+
+  // search
+  print("Search result");
+  var searchResults = Product.searchByName("Ap");
+  if (searchResults.isEmpty) {
+    print("Can not found");
+  } else {
+    searchResults.forEach((p) => print("${p.name} - ${p.price}"));
+  }
 }
+
+
